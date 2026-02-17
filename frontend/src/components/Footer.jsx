0e619e81facd2165a80bb2790690ec3884165f53
enzo-cys <enzo.cys@laplateforme.io>
2026-02-17 09:16:08 +0100

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// components/Footer.jsx
+
+function Footer() {
+  return (
+    <footer>
+      <p>Starter Kit - © {new Date().getFullYear()} La Plateforme_</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+// layouts/MainLayout.jsx
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet /> {/* ← La page enfant s'affiche ici */}
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
